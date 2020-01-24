47a60f2091fdf862fbf08840633f388aa6c01a8b
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2020-01-24 15:14:20 -0600

    Beginning site makeover

---
+++ scripts/common/common.js
@@ -0,0 +1,2 @@
+"use strict";
+// Any code here will be shared between the generator and the site itself
---
+++ scripts/site/placeholder.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * This is a file that will get removed once I start writting actual files for this project
+ */
+function helloWorld(one, two) {
+    console.log("Hello World! " + one * two);
+}
